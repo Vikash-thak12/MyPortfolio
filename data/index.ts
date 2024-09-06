46e7c6c69a76e09f29302f426b1be2a078ae1e3a
Vikash Thakur <thakvikash622@gmail.com>
2024-09-06 12:23:00 +0530

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,9 @@
+export const navItems = [
+    { name: "Home", link: "/" },
+    { name: "Profile", link: "/profile" },
+    { name: "Experience", link: "/experience" },
+    { name: "Projects", link: "/projects" },
+    // { name: "Contact", link: "/contact" },
+  ];
+
+  
