7d5a17a31eb918b36d70d60fd24aaf94b29104f7
Vikash Thakur <thakvikash622@gmail.com>
2025-08-17 21:46:37 +0530

    Final Commit

--- data/index.ts
+++ data/index.ts
@@ -1,57 +1,65 @@
 export const navItems = [
-    { name: "Home", link: "/" },
-    { name: "Profile", link: "/profile" },
-    { name: "Skills", link: "/skills" },
-    { name: "Projects", link: "/projects" },
-  ];
+  { name: "Home", link: "/" },
+  { name: "Profile", link: "/profile" },
+  { name: "Skills", link: "/skills" },
+  { name: "Projects", link: "/projects" },
+];
 
-  export const projects = [
-    {
-      id: 0,
-      title: "Expense Tracker",
-      des: "A React.js-based web application designed to help users manage their finances efficiently. It allows users to track their income, expenses, and overall balance through interactive charts and an intuitive UI.",
-      img: "/expense.png",
-      iconLists: ["/assets/javascript.svg", "/assets/mongodb.svg", "/assets/react.svg"],
-      link: "https://trackyourex.netlify.app/dashboard",
-    },
-    {
-      id: 1,
-      title: "Drive",
-      des: " A Google Drive clone built using advanced technologies, secure file uploads, file sharing, intuitive folder management, and seamless user authentication through OTP. This project offers efficient file storage and retrieval with a clean, user-friendly interface. ",
-      img: "/drive.png",
-      iconLists: ["/assets/next.svg", "/logo.png", "/assets/ts.svg"],
-      link: "https://drive-sigma-lac.vercel.app/",
-    },
-    {
-      id: 2,
-      title: "Zoom",
-      des: "A modern platform designed to provide seamless, real-time communication and collaboration. With features like high-quality video and audio, screen sharing, meeting recording, and secure authentication, it ensures a user-friendly experience across devices.",
-      img: "/zoom.png",
-      iconLists: ["/assets/next.svg", "/assets/c.svg", "/assets/ts.svg"],
-      link: "https://zoom-amber.vercel.app/",
-    },
-    {
-      id: 3,
-      title: "AI Trip Planner",
-      des: "An intelligent, AI-driven app designed to revolutionize your trip planning..",
-      img: "/trip.png",
-      iconLists: ["/assets/re.svg", "/assets/tail.svg", "/assets/c.svg"],
-      link: "https://trip-chi.vercel.app/",
-    },
-    {
-      id: 4,
-      title: "Portfolio",
-      des: "My Personal Portfolio to showcase my skills, projects with a beautiful User Interface",
-      img: "/portfolio.png",
-      iconLists: ["/assets/next.svg", "/assets/tail.svg", "/assets/ts.svg",],
-      link: "https://vikash-beta.vercel.app/",
-    },
-    {
-      id: 5,
-      title: "Image Revamp",
-      des: "A Web to Improve, renovate, or give a fresh and updated appearance to your Image.",
-      img: "/imagerevamp.png",
-      iconLists: ["/assets/next.svg", "/assets/c.svg", "/assets/mongo.svg"],
-      link: "https://imgrevamp.vercel.app/",
-    }
-  ];
+export const projects = [
+  {
+    id: 0,
+    title: "AI Resume Analyzer",
+    des: "AI Resume Analyzer is a Next.js app that uses Puter.js to upload and process resumes, providing AI-powered feedback on skills, formatting, and ATS compatibility.",
+    img: "/ai_resume_analyzer.png",
+    iconLists: ["/assets/react.svg", "/assets/tailwind.svg", "/assets/javascript.svg"],
+    link: "https://ai-resume-analyzer-hazel-six.vercel.app",
+  },
+  {
+    id: 1,
+    title: "Expense Tracker",
+    des: "A React.js-based web application designed to help users manage their finances efficiently. It allows users to track their income, expenses, and overall balance through interactive charts and an intuitive UI.",
+    img: "/expense.png",
+    iconLists: ["/assets/javascript.svg", "/assets/mongodb.svg", "/assets/react.svg"],
+    link: "https://trackyourex.netlify.app/dashboard",
+  },
+  {
+    id: 2,
+    title: "Drive",
+    des: " A Google Drive clone built using advanced technologies, secure file uploads, file sharing, intuitive folder management, and seamless user authentication through OTP. This project offers efficient file storage and retrieval with a clean, user-friendly interface. ",
+    img: "/drive.png",
+    iconLists: ["/assets/next.svg", "/logo.png", "/assets/ts.svg"],
+    link: "https://drive-sigma-lac.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Zoom",
+    des: "A modern platform designed to provide seamless, real-time communication and collaboration. With features like high-quality video and audio, screen sharing, meeting recording, and secure authentication, it ensures a user-friendly experience across devices.",
+    img: "/zoom.png",
+    iconLists: ["/assets/next.svg", "/assets/c.svg", "/assets/ts.svg"],
+    link: "https://zoom-amber.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "AI Trip Planner",
+    des: "An intelligent, AI-driven app designed to revolutionize your trip planning..",
+    img: "/trip.png",
+    iconLists: ["/assets/re.svg", "/assets/tail.svg", "/assets/c.svg"],
+    link: "https://trip-chi.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Portfolio",
+    des: "My Personal Portfolio to showcase my skills, projects with a beautiful User Interface",
+    img: "/portfolio.png",
+    iconLists: ["/assets/next.svg", "/assets/tail.svg", "/assets/ts.svg",],
+    link: "https://vikash-beta.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Image Revamp",
+    des: "A Web to Improve, renovate, or give a fresh and updated appearance to your Image.",
+    img: "/imagerevamp.png",
+    iconLists: ["/assets/next.svg", "/assets/c.svg", "/assets/mongo.svg"],
+    link: "https://imgrevamp.vercel.app/",
+  }
+];
